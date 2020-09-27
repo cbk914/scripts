49945b46da80ab5450f9b40071abe18143bd6051
cbk914 <cbk914@riseup.net>
2020-09-27 11:53:14 +0200

    Update

--- git_update_all.sh
+++ git_update_all.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 echo "[+] Updating Github directories..."
 echo "[+] =============================="
-#find . -maxdepth 1 -type d -print -execdir echo git --git-dir={}/.git --work-tree=$PWD/{} fetch origin master:master \;
-#content=`ls -l | grep '^d'`
-#for dir in $content;
-#do 
-#	cd $dir; git pull; cd ..;
-#done
 
 for i in */.git; do ( echo $i; cd $i/..; git pull; ); done
 
