8ae4b7d0c430fc30f5a1978dedbd13dcf537fed3
cbk914 <cbk914@riseup.net>
2020-09-27 12:03:53 +0200

    Update

--- git_update_all.sh
+++ git_update_all.sh
@@ -2,7 +2,7 @@
 echo "[+] Updating Github directories..."
 echo "[+] =============================="
 
-for i in */.git; do ( echo $i; cd $i/..; git pull; ); done
+for i in */.git; do ( echo $i; cd $i/..; git branch; git pull; ); done
 
 
 echo "[+] Update finished"
