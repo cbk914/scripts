c7f65afb5030d66f5d6001ab3ceebaf1ae7680de
cbk914 <cbk914@riseup.net>
2020-10-11 19:07:47 +0200

    Update

--- backup_firmware.sh
+++ backup_firmware.sh
@@ -1,10 +1,15 @@
 #!/bin/ash
-# Simple script to backup RAM to binary file
+# Simple script to backup RAM <> binary file
 # Execute on OpenWRT system
 FILE="/tmp/backup_firmware.bin"
 
 touch $FILE && 
 cd /tmp &&
+# u-boot
+# cat /dev/mtd0 > /tmp/backup_u-boot.bin
+# Art
+# cat /dev/mtd4 > /tmp/backup_art.bin
+# Firmware + settings
 cat /dev/mtd5 > $FILE
 #scp $FILE <destination>
 # To restore:
