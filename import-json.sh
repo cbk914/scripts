d57848754c081bc04a589c9ac94522eb6dd9d6cc
cbk914 <cbk914@riseup.net>
2023-01-24 17:47:58 +0100

    Update import-json.sh

--- import-json.sh
+++ import-json.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
-# Author cbk914
+# Author: cbk914
 function display_help {
-    echo "Usage: $0 -f file -u url [-p proxy] [-h]"
+    echo "Usage: $0 -f file -u url [-o output] [-p proxy] [-h]"
     echo "Retrieves URLs from a JSON file and sends a request to each URL using curl"
     echo "  -f file         path to the JSON file"
     echo "  -u url          base URL to append the URI to"
+    echo "  -o output       specify the output file name"
     echo "  -p proxy        send URL requests to proxy"
     echo "  -h              display this help and exit"
     exit 0
 }
 
-while getopts "f:u:p:h" opt; do
+while getopts "f:u:o:p:h" opt; do
   case $opt in
     f) file="$OPTARG"
     ;;
     u) url_base="$OPTARG"
+    ;;
+    o) output="$OPTARG"
     ;;
     p) proxy="$OPTARG"
     ;;
@@ -49,9 +52,39 @@ while IFS='' read -r line || [[ -n "$line" ]]; do
     fi
     # Use curl to send a request to the URL
     if [ -z "$proxy" ] || [ "$proxy" == "null" ]; then
-        curl -k -L $url
+        if [ -n "$output" ]; then
+            response_code=$(curl -o /dev/null --silent --head --write-out '%{http_code}' $url)
+            echo "URL: $url Response Code: $response_code" >> $output
+            if [ $response_code -eq 200 ]; then
+                echo "URL: $url Response Code: $response_code" >> $output-200
+            elif [ $response_code -eq 404 ]; then
+                echo "URL: $url Response Code: $response_code" >> $output-404
+            elif [ $response_code -eq 500 ]; then
+                echo "URL: $url Response Code: $response_code" >> $output-500
+            else
+                echo "URL: $url Response Code: $response_code" >> $output-other
+            fi
+        else
+            curl -k -L $url
+        fi
     else
-        curl -k -L -x $proxy $url
+        if [ -n "$output" ]; then
+            response_code=$(curl -x $proxy -o /dev/null --silent --head --write-out '%{http_code}' $url)
+            echo "URL: $url Response Code: $response_code" >> $output
+            if [ $response_code -eq 200 ]; then
+                echo "URL: $url Response Code: $response_code" >> $output-200
+            elif [ $response_code -eq 404 ]; then
+                echo "URL: $url Response Code: $response_code" >> $output-404
+			elif [ $response_code -eq 403 ]; then
+                echo "URL: $url Response Code: $response_code" >> $output-403
+            elif [ $response_code -eq 500 ]; then
+                echo "URL: $url Response Code: $response_code" >> $output-500
+            else
+                echo "URL: $url Response Code: $response_code" >> $output-other
+            fi
+        else
+            curl -k -L -x $proxy $url
+        fi
     fi
     if [ $? -ne 0 ]; then
         echo "Error: curl command failed"
