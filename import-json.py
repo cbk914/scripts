7940643373a5dd8117de5bbd66036cb9b8ec5ca5
cbk914 <cbk914@riseup.net>
2023-01-24 20:40:07 +0100

    Update import-json.py

--- import-json.py
+++ import-json.py
@@ -3,9 +3,10 @@
 import argparse
 import json
 import requests
+from requests.structures import CaseInsensitiveDict
 
 def display_help():
-    print("Usage: python import-json.py -f file -u url [-o output] [-p proxy] [-h]")
+    print("Usage: python script.py -f file -u url [-o output] [-p proxy] [-h]")
     print("Retrieves URLs from a JSON file and sends a request to each URL using requests")
     print("  -f file         path to the JSON file")
     print("  -u url          base URL to append the URI to")
@@ -26,7 +27,9 @@ if not args.file or not args.url:
     print("Error: -f and -u options are required")
     exit(1)
 
-urls = []
+# Use a set to store unique URLs
+urls = set()
+
 # Iterate through each line of the file
 with open(args.file) as f:
     for line in f:
@@ -34,37 +37,46 @@ with open(args.file) as f:
         uri = json.loads(line)['result']['uri']
         # Construct the full URL
         url = args.url + uri
-        # Remove duplicates
-        if url not in urls:
-            # Print the URL
-            print(url)
-            urls.append(url)
-        else:
-            print("Duplicate URL skipped.")
-        # Use requests to send a request to the URL
-        if args.proxy:
-            response = requests.get(url, proxies={'http': args.proxy, 'https': args.proxy})
-        else:
-            response = requests.get(url)
-        response_code = response.status_code
-        #write to file
+        # Add the URL to the set of unique URLs
+        urls.add(url)
+
+# Iterate through the unique URLs
+for url in urls:
+    # Print the URL
+    print(url)
+    headers = CaseInsensitiveDict()
+    headers["User-Agent"] = "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:123.0) Gecko/20100101 Firefox/123.0"
+    headers["Accept-Language"] = "en-US,en;q=0.5"
+    headers["Accept-Encoding"] = "gzip, deflate"
+    headers["Connection"] = "close"
+
+    # Use requests to send a request to the URL
+    if args.proxy:
+        response = requests.get(url, headers=headers, proxies={'http': args.proxy, 'https': args.proxy})
+    else:
+        response = requests.get(url, headers=headers)
+
+    response_code = response.status_code
+
+    # Write to file
         if args.output:
-            with open(args.output, 'a') as file:
+        with open(args.output, 'a') as file:
+            file.write(f"URL: {url} Response Code: {response_code}\n")
+        if response_code == 200:
+            with open(args.output+'-200', 'a') as file:
                 file.write(f"URL: {url} Response Code: {response_code}\n")
-            if response_code == 200:
-                with open(args.output+'-200', 'a') as file:
-                    file.write(f"URL: {url} Response Code: {response_code}\n")
-            elif response_code == 404:
-                with open(args.output+'-404', 'a') as file:
-                    file.write(f"URL: {url} Response Code: {response_code}\n")
-			elif response_code == 403:
-				with open(args.output+'-403', 'a') as file:
-                    file.write(f"URL: {url} Response Code: {response_code}\n")
-            elif response_code == 500:
-                with open(args.output+'-500', 'a') as file:
-                    file.write(f"URL: {url} Response Code: {response_code}\n")
-            else:
-                with open(args.output+'-other', 'a') as file:
-                    file.write(f"URL: {url} Response Code: {response_code}\n")
-        # Print the response code
-        print(f"Response Code: {response_code}")
+        elif response_code == 404:
+            with open(args.output+'-404', 'a') as file:
+                file.write(f"URL: {url} Response Code: {response_code}\n")
+		elif response_code == 403:
+			with open(args.output+'-403', 'a') as file:
+                file.write(f"URL: {url} Response Code: {response_code}\n")
+        elif response_code == 500:
+            with open(args.output+'-500', 'a') as file:
+                file.write(f"URL: {url} Response Code: {response_code}\n")
+        else:
+            with open(args.output+'-other', 'a') as file:
+                file.write(f"URL: {url} Response Code: {response_code}\n")
+
+    # Print the response code
+    print(f"Response Code: {response_code}")
