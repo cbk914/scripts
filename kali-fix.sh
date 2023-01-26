157ce3dca2c53b18a9f55bb3ac33d504ccf890e2
cbk914 <cbk914@riseup.net>
2023-01-26 21:21:36 +0100

    Update kali-fix.sh

--- kali-fix.sh
+++ kali-fix.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+# Check for new keyrings after expiration: https://pkg.kali.org/pkg/kali-archive-keyring
 wget https://ftp.cc.uoc.gr/mirrors/linux/kali/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2022.1_all.deb
 apt install ./kali-archive-keyring_2022.1_all.deb
 apt-get update
