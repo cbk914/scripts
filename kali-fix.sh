814320ac5965fb16cc1d71fc080bc216071a818d
cbk914 <cbk914@riseup.net>
2023-01-27 10:52:41 +0100

    Update kali-fix.sh

--- kali-fix.sh
+++ kali-fix.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 # Check for new keyrings after expiration: https://pkg.kali.org/pkg/kali-archive-keyring
 # Another option - sudo wget https://archive.kali.org/archive-key.asc -O /etc/apt/trusted.gpg.d/kali-archive-keyring.asc
-wget https://ftp.cc.uoc.gr/mirrors/linux/kali/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2022.1_all.deb
-apt install ./kali-archive-keyring_2022.1_all.deb
+gpg --keyserver keyserver.ubuntu.com --recv-key 7D8D0BF6
+gpg --fingerprint 7D8D0BF6
+gpg -a --export 7D8D0BF6 | apt-key add -
+# wget https://ftp.cc.uoc.gr/mirrors/linux/kali/kali/pool/main/k/kali-archive-keyring/kali-archive-keyring_2022.1_all.deb
+# apt install ./kali-archive-keyring_2022.1_all.deb
 apt-get update
