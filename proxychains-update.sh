45b68621a8b4c317c13112092db404cc975f88e8
David Espejo <david@fortytwo.nl>
2024-06-17 14:27:04 +0200

    Updated to proxychains4

--- proxychains-update.sh
+++ proxychains-update.sh
@@ -26,20 +26,34 @@ echo "" >> $PROXYCHAINS_CONF
 # Add HTTP Proxies
 echo "[ProxyList]" >> $PROXYCHAINS_CONF
 echo "# HTTP Proxies" >> $PROXYCHAINS_CONF
-curl -s $HTTP_URL | head -n 10 | awk '{print "http " $1 " " $2}' >> $PROXYCHAINS_CONF
+curl -s $HTTP_URL | head -n 10 | while read -r line; do
+    ip=$(echo $line | cut -d ':' -f 1)
+    port=$(echo $line | cut -d ':' -f 2)
+    echo "http $ip $port" >> $PROXYCHAINS_CONF
+done
 
 # Add SOCKS4 Proxies
 echo "" >> $PROXYCHAINS_CONF
 echo "# SOCKS4 Proxies" >> $PROXYCHAINS_CONF
-curl -s $SOCKS4_URL | head -n 10 | awk '{print "socks4 " $1 " " $2}' >> $PROXYCHAINS_CONF
+curl -s $SOCKS4_URL | head -n 10 | while read -r line; do
+    ip=$(echo $line | cut -d ':' -f 1)
+    port=$(echo $line | cut -d ':' -f 2)
+    echo "socks4 $ip $port" >> $PROXYCHAINS_CONF
+done
 
 # Add SOCKS5 Proxies
 echo "" >> $PROXYCHAINS_CONF
 echo "# SOCKS5 Proxies" >> $PROXYCHAINS_CONF
-curl -s $SOCKS5_URL | head -n 10 | awk '{print "socks5 " $1 " " $2}' >> $PROXYCHAINS_CONF
+curl -s $SOCKS5_URL | head -n 10 | while read -r line; do
+    ip=$(echo $line | cut -d ':' -f 1)
+    port=$(echo $line | cut -d ':' -f 2)
+    echo "socks5 $ip $port" >> $PROXYCHAINS_CONF
+done
 
 echo "" >> $PROXYCHAINS_CONF
 echo "# End of Proxy List" >> $PROXYCHAINS_CONF
+echo "Updating proxychains.conf"
+sudo cp proxychains.conf /opt/homebrew/etc/proxychains.conf
 
 echo "proxychains.conf file updated with the latest 10 proxies from each list."
 
